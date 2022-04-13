1b64cbb689432223c106ca432fc2a6811ceef747
Pol Reig <polreigbroto@gmail.com>
2022-04-13 16:27:52 +0200

    added integration tests for retweet mutation

--- __tests__/steps/then.js
+++ __tests__/steps/then.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
+const _ = require('lodash')
 const AWS = require('aws-sdk')
 const http = require('axios')
+const fs = require('fs')
 
 const user_exists_in_UsersTable = async (id) => {
   const DynamoDB = new AWS.DynamoDB.DocumentClient()
@@ -40,8 +42,121 @@ const user_can_download_image_from = async (url) => {
   return resp.data
 }
 
+const tweet_exists_in_TweetsTable = async (id) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+  console.log(`looking for tweet [${id}] in table [${process.env.TWEETS_TABLE}]`)
+  const resp = await DynamoDB.get({
+    TableName: process.env.TWEETS_TABLE,
+    Key: {
+      id
+    }
+  }).promise()
+
+  expect(resp.Item).toBeTruthy()
+
+  return resp.Item
+}
+
+const retweet_exists_in_TweetsTable = async (userId, tweetId) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+  console.log(`looking for retweet of [${tweetId}] in table [${process.env.TWEETS_TABLE}]`)
+  const resp = await DynamoDB.query({
+    TableName: process.env.TWEETS_TABLE,
+    IndexName: 'retweetsByCreator',
+    KeyConditionExpression: 'creator = :creator AND retweetOf = :tweetId',
+    ExpressionAttributeValues: {
+      ':creator': userId,
+      ':tweetId': tweetId
+    },
+    Limit: 1
+  }).promise()
+
+  const retweet = _.get(resp, 'Items.0')
+
+  expect(retweet).toBeTruthy()
+
+  return retweet
+}
+
+const retweet_exists_in_RetweetsTable = async (userId, tweetId) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+  console.log(`looking for retweet of [${tweetId}] for user [${userId}] in table [${process.env.RETWEETS_TABLE}]`)
+  const resp = await DynamoDB.get({
+    TableName: process.env.RETWEETS_TABLE,
+    Key: {
+      userId,
+      tweetId
+    }
+  }).promise()
+
+  expect(resp.Item).toBeTruthy()
+
+  return resp.Item
+}
+
+const tweet_exists_in_TimelinesTable = async (userId, tweetId) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+  console.log(`looking for tweet [${tweetId}] for user [${userId}] in table [${process.env.TIMELINES_TABLE}]`)
+  const resp = await DynamoDB.get({
+    TableName: process.env.TIMELINES_TABLE,
+    Key: {
+      userId,
+      tweetId
+    }
+  }).promise()
+
+  expect(resp.Item).toBeTruthy()
+
+  return resp.Item
+}
+
+const there_are_N_tweets_in_TimelinesTable = async (userId, n) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+  console.log(`looking for [${n}] tweets for user [${userId}] in table [${process.env.TIMELINES_TABLE}]`)
+  const resp = await DynamoDB.query({
+    TableName: process.env.TIMELINES_TABLE,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    },
+    ScanIndexForward: false
+  }).promise()
+
+  expect(resp.Items).toHaveLength(n)
+
+  return resp.Items
+}
+
+const tweetsCount_is_updated_in_UsersTable = async (id, newCount) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+  console.log(`looking for user [${id}] in table [${process.env.USERS_TABLE}]`)
+  const resp = await DynamoDB.get({
+    TableName: process.env.USERS_TABLE,
+    Key: {
+      id
+    }
+  }).promise()
+
+  expect(resp.Item).toBeTruthy()
+  expect(resp.Item.tweetsCount).toEqual(newCount)
+
+  return resp.Item
+}
+
 module.exports = {
   user_exists_in_UsersTable,
   user_can_upload_image_to_url,
-  user_can_download_image_from
+  user_can_download_image_from,
+  tweet_exists_in_TweetsTable,
+  tweet_exists_in_TimelinesTable,
+  retweet_exists_in_TweetsTable,
+  there_are_N_tweets_in_TimelinesTable,
+  retweet_exists_in_RetweetsTable,
+  tweetsCount_is_updated_in_UsersTable
 }
--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -127,6 +127,38 @@ const we_invoke_getImageUploadUrl = async (username, extension, contentType) => 
   return await handler(event, context)
 }
 
+const we_invoke_tweet = async (username, text) => {
+  const handler = require('../../functions/tweet').handler
+
+  const context = {}
+  const event = {
+    identity: {
+      username
+    },
+    arguments: {
+      text
+    }
+  }
+
+  return await handler(event, context)
+}
+
+const we_invoke_retweet = async (username, tweetId) => {
+  const handler = require('../../functions/retweet').handler
+
+  const context = {}
+  const event = {
+    identity: {
+      username
+    },
+    arguments: {
+      tweetId
+    }
+  }
+
+  return await handler(event, context)
+}
+
 const a_user_signs_up = async (password, name, email) => {
   const cognito = new AWS.CognitoIdentityServiceProvider()
 
@@ -354,6 +386,8 @@ const a_user_calls_getLike = async (user, userId, limit , nextToken) => {
 module.exports = {
   we_invoke_confirmUserSignup,
   we_invoke_getImageUploadUrl,
+  we_invoke_tweet,
+  we_invoke_retweet,
   a_user_signs_up,
   we_invoke_an_appsync_template,
   a_user_calls_getMyProfile,
